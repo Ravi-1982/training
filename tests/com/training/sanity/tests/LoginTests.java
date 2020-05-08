c9d90fbc1f647ff977e8952471b64837e8c5e1a9
Ravi-1982 <ravguntu@in.ibm.com>
2020-05-08 08:10:59 +0530

    push

---
+++ tests/com/training/sanity/tests/LoginTests.java
@@ -0,0 +1,66 @@
+package com.training.sanity.tests;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.training.generics.ScreenShot;
+import com.training.pom.LoginPOM;
+import com.training.utility.DriverFactory;
+import com.training.utility.DriverNames;
+
+public class LoginTests {
+
+	private static WebDriver driver;
+	private static String baseUrl;
+	private static LoginPOM loginPOM;
+	private static Properties properties;
+	private static ScreenShot screenShot;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws IOException {
+		properties = new Properties();
+		FileInputStream inStream = new FileInputStream("./resources/others.properties");
+		properties.load(inStream);
+		driver = DriverFactory.getDriver(DriverNames.CHROME);
+		loginPOM = new LoginPOM(driver); 
+		baseUrl = properties.getProperty("baseUrl");
+		screenShot = new ScreenShot(driver); 
+		// open the browser 
+		driver.get(baseUrl);
+	}
+
+/*	@BeforeMethod
+	public void setUp() throws Exception {
+	
+	}
+	
+	@BeforeSuite
+	public void openBrowser() {
+	System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
+		        driver = new ChromeDriver();
+		        driver.get("baseUrl");
+	}*/
+	
+	@Test
+	public void validLoginTest() {
+		loginPOM.sendUserName("admin");
+		loginPOM.sendPassword("admin@123");
+		loginPOM.clickLoginBtn(); 
+		screenShot.captureScreenShot("First");
+	}
+	/*@AfterMethod
+	public void tearDown() throws Exception {
+		Thread.sleep(1000);
+		driver.quit();
+	}*/
+	
+}
